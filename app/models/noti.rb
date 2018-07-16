d4f2a36816b4f7e1f28eaa15f0d317bce50c5c38
juyeoungchris <juyeoung.kim@nbnl.co>
2018-07-16 17:04:45 +0900

    id version

--- app/models/noti.rb
+++ app/models/noti.rb
@@ -1,6 +1,6 @@
 class Noti < ApplicationRecord
 	belongs_to :user 
 	# belongs_to :wysiwyg
-	has_attached_file :image,:default_url => "missing.png", styles: { medium:"200x200>"} 
+	has_attached_file :image,:default_url => "missing.png", styles: { medium:"200x200>" , small: "100x100>"} 
 	validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
 end
